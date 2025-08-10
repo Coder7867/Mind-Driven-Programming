7910f153de3d9b4b7964aacc88a2233ea6a50890
Amina Ashfaq <161596506+Coder7867@users.noreply.github.com>
2025-08-10 16:50:53 +0500

    Update Dream Debugger.py

--- pages/Dream Debugger.py
+++ pages/Dream Debugger.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils import save_data, load_data
 import uuid
 st.title("Dream Debugger")
 
